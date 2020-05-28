b0dc803af2c19162ba12374d7414c5c312bf7a1a
alexander <alexander.barros.orellana@gmail.com>
2020-05-27 23:28:47 -0400

    test: add ui kitten components to ignore patterns

--- src/utils/testing.js
+++ src/utils/testing.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import { AuthProvider } from '../context/AuthContext';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { render } from '@testing-library/react-native';
+import * as eva from '@eva-design/eva';
+import { default as theme } from '../../custom-theme.json';
 
 export const renderWithAuthProvider = ({ ...children }) =>
-  render(<AuthProvider>{children}</AuthProvider>);
+  render(
+    <AuthProvider>
+      <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
+        {children}
+      </ApplicationProvider>
+    </AuthProvider>,
+  );
+
+// export const renderWith

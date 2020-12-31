91536daffbe9e72bed660c00486275b6bf7308b0
Matias Barrera <matias.barrera95@gmail.com>
2020-12-30 22:52:55 -0300

    fix: removed mapping in testing.js

--- src/utils/testing.js
+++ src/utils/testing.js
@@ -5,7 +5,6 @@ import { ApplicationProvider, IconRegistry } from '@ui-kitten/components'
 import { render } from '@testing-library/react-native'
 import * as eva from '@eva-design/eva'
 import { default as theme } from '../../custom-theme.json'
-import { default as mapping } from '../../mapping.json'
 import { EvaIconsPack } from '@ui-kitten/eva-icons'
 import { MaterialIconsPack, FeatherIconsPack } from '../../icons'
 import { NavigationContainer } from '@react-navigation/native'
@@ -19,10 +18,7 @@ export const renderWithProviders = ({ ...children }) =>
         <IconRegistry
           icons={[EvaIconsPack, FeatherIconsPack, MaterialIconsPack]}
         />
-        <ApplicationProvider
-          {...eva}
-          theme={{ ...eva.light, ...theme }}
-          customMapping={mapping}>
+        <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
           {children}
         </ApplicationProvider>
       </AuthProvider>

48e4878bce26c05f449b96d913e875ded91a57c0
Alexander Barros <alexander.barros.orellana@gmail.com>
2021-01-17 01:41:20 -0300

    chore: change home name to fed

--- env_setter.sh
+++ env_setter.sh
@@ -12,7 +12,7 @@ elif [ "$1" = "local" ]
 then
     touch .env
     echo "MOBILE_NODE_ENV=local" >> .env
-    ip=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
-    ipLocal="$ip"
-    echo "MOBILE_IP=${ipLocal[0]}" >> .env
+    # localIps=$(ip address show | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
+    ipLocal="$(ip -o route get to 8.8.8.8 | sed -n 's/.*src \([0-9.]\+\).*/\1/p')"
+    echo "MOBILE_IP=${ipLocal}" >> .env
 fi
